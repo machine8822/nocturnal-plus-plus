8b4cae27e73a70ffe7757ada1c7267c1b62382c1
[reekoooooooo] <[ltyrek1@gmail.com]>
2026-02-26 19:21:08 -0500

    Add DataLoader stub

---
+++ src/main/java/com/model/DataLoader.java
@@ -0,0 +1,30 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class DataLoader {
+
+    private String basePath;
+
+    public DataLoader(String basePath) {
+        this.basePath = basePath;
+    }
+
+    public ArrayList<User> loadUsers() {
+        // TODO: Implement JSON reading later
+        System.out.println("Stub: loadUsers called");
+        return new ArrayList<>();
+    }
+
+    public ArrayList<InterviewQuestion> loadQuestions() {
+        // TODO: Implement JSON reading later
+        System.out.println("Stub: loadQuestions called");
+        return new ArrayList<>();
+    }
+
+    public ArrayList<QuestionSubmission> loadSubmissions() {
+        // TODO: Implement JSON reading later
+        System.out.println("Stub: loadSubmissions called");
+        return new ArrayList<>();
+    }
+}
